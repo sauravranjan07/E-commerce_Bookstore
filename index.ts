70cd9e2b1a982b33e1717fb88a6da6c1dde24f04
geekysaurav007 <108987930+geekysaurav007@users.noreply.github.com>
2024-03-07 12:00:05 +0530

    Database connection created

--- index.ts
+++ index.ts
@@ -1,8 +1,14 @@
 import express,{Express,Response,Request} from 'express'
+import {createConnection} from './Database/connection'
+import morgan  from "morgan"
 const app:Express=express()
+app.use(morgan('dev'))
 
-app.listen(3000,()=>{
-    console.log(`helooo working on port 30000`)
+app.listen(3000,async():Promise<any>=>{
+   let result:string|undefined= await createConnection()
+    if(result){
+        console.log(`I am ${result} and working on port 3000`)
+    }
    
 })
 app.get('/',(req:Request,resp:Response)=>{
