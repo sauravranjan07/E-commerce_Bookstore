477bcd0e490f7bf24b1eadaaea714561643da5de
geekysaurav007 <108987930+geekysaurav007@users.noreply.github.com>
2024-03-19 22:52:26 +0530

    minor refactoring

--- Routes/orderRoutes.ts
+++ Routes/orderRoutes.ts
@@ -3,6 +3,6 @@ const orderRouter=express.Router()
 import {userAuthMiddleware} from '../middlewares/user-auth-middleware'
 import {createOrder,deleteMyOrder,getMyOrder} from "../controllers/orderController"
 orderRouter.post('/placeorder',createOrder)
-orderRouter.get('/myorder',userAuthMiddleware,getMyOrder)
+orderRouter.get('/myorders',userAuthMiddleware,getMyOrder)
 orderRouter.delete('/delete/:id',deleteMyOrder)
 export{orderRouter}
