a444e50656267709d6f8ff42a7737a0c0793c7a8
geekysaurav007 <108987930+geekysaurav007@users.noreply.github.com>
2024-04-05 21:41:56 +0530

    added middlewares while accessing role absed routes

--- Routes/orderRoutes.ts
+++ Routes/orderRoutes.ts
@@ -2,7 +2,7 @@ import express from 'express'
 const orderRouter=express.Router()
 import {userAuthMiddleware} from '../middlewares/user-auth-middleware'
 import {createOrder,deleteMyOrder,getMyOrder} from "../controllers/orderController"
-orderRouter.post('/placeorder',createOrder)
+orderRouter.post('/placeorder',userAuthMiddleware,createOrder)
 orderRouter.get('/myorders',userAuthMiddleware,getMyOrder)
-orderRouter.delete('/delete/:id',deleteMyOrder)
+orderRouter.delete('/delete/:id',userAuthMiddleware,deleteMyOrder)
 export{orderRouter}
