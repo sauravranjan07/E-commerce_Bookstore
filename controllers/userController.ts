c8e17ab4825a06d935d366fbbeaa662f948b3889
geekysaurav007 <108987930+geekysaurav007@users.noreply.github.com>
2024-05-06 22:05:04 +0530

    mail refactor and added total price in Mail & refactored-3

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -102,14 +102,14 @@ async function loginUser(
       });
     }
     const result = await userSchema.findOne({ email: email });
-    let mail_error={
-      message: "Email dosent exist",
-      success: false,
-      token: "Null",
-    }
+    
     if (!result) {
       resp.statusCode = 404;
-      throw new Error(JSON.stringify(mail_error))
+      return resp.json({
+        message: "Email dosent exist",
+        success: false,
+        token: "Null",
+      })
     }
     // 2.if password not matched
     const verifiedPassword = hashPasswordHelper.verify(
