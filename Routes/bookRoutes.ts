25eb709c778dad1575a365fbf740099905e1be1f
geekysaurav007 <108987930+geekysaurav007@users.noreply.github.com>
2024-10-28 01:20:45 +0530

    added multer s3 bucket & removed cred

--- Routes/bookRoutes.ts
+++ Routes/bookRoutes.ts
@@ -9,41 +9,41 @@ import {
 import { adminAuthMiddleware } from "../middlewares/user-auth-middleware";
 import multer from "multer";
 import path from "path";
+import * as dotenv from 'dotenv';
+dotenv.config();
 import mongoose from "mongoose";
-let upload_Folder = "media/products";
-let s3_key="AKIA6JQ45DI2D3K5HVKJ";
-let s3_secret_key="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"
-let s3_bucket_name="pageturnbooksimg"
 
-const { S3Client } = require('@aws-sdk/client-s3')
+import { S3Client } from "@aws-sdk/client-s3";
 // const multerS3 = require('multer-s3')
-import multers3 from  'multer-s3'
+import multers3 from "multer-s3";
 
-const app = express()
+const app = express();
+console.log(process.env.s3_secret_key);
+console.log(process.env.s3_key);
 
 const s3 = new S3Client({
-  region:"ap-southeast-2",
-  credentials:{
-    secretAccessKey:s3_secret_key,
-    accessKeyId:s3_key,
+  region: "ap-southeast-2",
+  credentials: {
+    accessKeyId:String(process.env.s3_key),
+    secretAccessKey:String(process.env.s3_secret_key),
   },
-})
-
-
-
-
+});
 
 // const tempMulter = multerS3({ dest: "media/products" });
 let storage = multers3({
   s3: s3,
-  bucket: s3_bucket_name,
-  metadata: function (req: any, file: { fieldname: any; }, cb: (arg0: null, arg1: { fieldName: any; }) => void) {
-    cb(null, {fieldName: file.fieldname});
+  bucket: "pageturnbooksimg",
+  metadata: function (
+    req: any,
+    file: { fieldname: any },
+    cb: (arg0: null, arg1: { fieldName: any }) => void
+  ) {
+    cb(null, { fieldName: file.fieldname });
   },
   key: function (req: any, file: any, cb: (arg0: null, arg1: string) => void) {
-    cb(null, new mongoose.Types.ObjectId() + ".png")
-  }
-})
+    cb(null, new mongoose.Types.ObjectId() + ".png");
+  },
+});
 const upload = multer({ storage });
 
 bookRouter.post("/addbook", upload.single("image"), createBook);
